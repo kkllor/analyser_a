9812cbcc713b91d1191ad29173e6bfcf113ea5c8
gaokai <zhanggaokai@hecom.cn>
2020-04-29 18:22:38 +0800

    持续接收指令

--- src/main/java/com/kkllor/Entrance.java
+++ src/main/java/com/kkllor/Entrance.java
@@ -13,42 +13,48 @@ public class Entrance {
     public static void main(String args[]) {
         Config.getInstance().parse();
         Scanner scanner = new Scanner(System.in);
-        String operation = inputCheck(scanner, "input 'download' or 'analyse' ,'collect' for following operations ", "unknown operation", str -> str.equals("download") || str.equals("analyse") || str.equals("collect"));
-        if ("download".equals(operation)) {
-            String codes = inputCheck(scanner, "input stock codes,split with ',' ", "unknown operation", str -> str.split(",").length > 0);
-            String years = inputCheck(scanner, "input download year,split with ',' ", "unknown years", str -> str.split(",").length > 0);
-            String[] a1 = years.split(",");
-            int[] a2 = new int[a1.length];
-            for (int i = 0; i < a1.length; i++) {
-                a2[i] = Integer.parseInt(a1[i]);
-            }
+        while (true) {
+            String operation = inputCheck(scanner, "input 'download' or 'analyse' ,'collect' ,'exit' for following operations ", "unknown operation", str -> str.equals("download") || str.equals("analyse") || str.equals("collect") || str.equals("exit"));
+            if ("download".equals(operation)) {
+                String codes = inputCheck(scanner, "input stock codes,split with ',' ", "unknown operation", str -> str.split(",").length > 0);
+                String years = inputCheck(scanner, "input download year,split with ',' ", "unknown years", str -> str.split(",").length > 0);
+                String[] a1 = years.split(",");
+                int[] a2 = new int[a1.length];
+                for (int i = 0; i < a1.length; i++) {
+                    a2[i] = Integer.parseInt(a1[i]);
+                }
 
-            String reportType = inputCheck(scanner, "download yearly reports input 'y',or 'q' for quater reports", "unknown years", str -> str.equals("y") || str.equals("q"));
-            ReportType reportType1;
-            if ("y".equals(reportType)) {
-                reportType1 = ReportType.YEAR;
-            } else if ("q".equals(reportType)) {
-                reportType1 = ReportType.QUARTER;
-            } else {
-                reportType1 = ReportType.OTHER;
-            }
+                String reportType = inputCheck(scanner, "download yearly reports input 'y',or 'q' for quater reports", "unknown years", str -> str.equals("y") || str.equals("q"));
+                ReportType reportType1;
+                if ("y".equals(reportType)) {
+                    reportType1 = ReportType.YEAR;
+                } else if ("q".equals(reportType)) {
+                    reportType1 = ReportType.QUARTER;
+                } else {
+                    reportType1 = ReportType.OTHER;
+                }
 
-            Map<String, String[]> codesMap = groupCodes(codes.split(","));
-            if (codesMap.get("sh").length > 0) {
-                SHDownLoader.getInstance().downloadByCodes(reportType1, a2, codesMap.get("sh"));
+                Map<String, String[]> codesMap = groupCodes(codes.split(","));
+                if (codesMap.get("sh").length > 0) {
+                    SHDownLoader.getInstance().downloadByCodes(reportType1, a2, codesMap.get("sh"));
+
+                }
+
+                if (codesMap.get("sz").length > 0) {
+                    SZDownLoader.getInstance().downloadByCodes(reportType1, a2, codesMap.get("sz"));
+
+                }
+
+            } else if ("collect".equals(operation)) {
+                String code = inputCheck(scanner, "input stock code ", "unknown operation", str -> str.split(",").length > 0);
+                new Analyser().collectData(code);
+            } else if ("analyse".equals(operation)) {
+                new Analyser().analyse("600300");
+            } else if ("exit".equals(operation)) {
                 SHDownLoader.getInstance().exit();
-            }
-
-            if (codesMap.get("sz").length > 0) {
-                SZDownLoader.getInstance().downloadByCodes(reportType1, a2, codesMap.get("sz"));
                 SZDownLoader.getInstance().exit();
+                System.exit(0);
             }
-
-        } else if ("collect".equals(operation)) {
-            String code = inputCheck(scanner, "input stock code ", "unknown operation", str -> str.split(",").length > 0);
-            new Analyser().collectData(code);
-        } else if ("analyse".equals(operation)) {
-            new Analyser().analyse("600300");
         }
     }
 
